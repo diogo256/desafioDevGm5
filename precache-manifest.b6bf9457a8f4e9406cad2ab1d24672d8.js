29678b25e1420701e6149c2cf915404cb44743be
Diogo Oliveira <diogosoliv5@gmail.com>
2019-07-29 04:10:21 -0300

    Updates

---
+++ precache-manifest.b6bf9457a8f4e9406cad2ab1d24672d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc1f1c84f91b578ef6c8a7889f9c2b1",
+    "url": "/desafioDevGm5/index.html"
+  },
+  {
+    "revision": "e47dc8f4f0fcdf109448",
+    "url": "/desafioDevGm5/static/css/main.90512d72.chunk.css"
+  },
+  {
+    "revision": "c4095113ae126447a826",
+    "url": "/desafioDevGm5/static/js/2.e34fdfaa.chunk.js"
+  },
+  {
+    "revision": "e47dc8f4f0fcdf109448",
+    "url": "/desafioDevGm5/static/js/main.c0981775.chunk.js"
+  },
+  {
+    "revision": "f3982faebe8e58259ed4",
+    "url": "/desafioDevGm5/static/js/runtime~main.bacfe355.js"
+  },
+  {
+    "revision": "1b4e59cb1c13a8c55c3fe55031afd3d9",
+    "url": "/desafioDevGm5/static/media/banner.1b4e59cb.jpg"
+  },
+  {
+    "revision": "53757d9306796cd8c19df677d30a10c7",
+    "url": "/desafioDevGm5/static/media/facebook-f-brands.53757d93.svg"
+  },
+  {
+    "revision": "e75317fafc5e835195adf92be513d35b",
+    "url": "/desafioDevGm5/static/media/instagram-brands.e75317fa.svg"
+  },
+  {
+    "revision": "1e01f0c0dea217ba568ebbcec49c5b6b",
+    "url": "/desafioDevGm5/static/media/linkedin-in-brands.1e01f0c0.svg"
+  },
+  {
+    "revision": "6620c8ae2cac90067a9aa78339bb808d",
+    "url": "/desafioDevGm5/static/media/twitter-brands.6620c8ae.svg"
+  }
+]);
