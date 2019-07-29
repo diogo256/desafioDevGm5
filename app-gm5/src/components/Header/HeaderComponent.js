f7017231e3afbb3a3137ed9794607f037440cf98
Diogo Oliveira <diogo.oliveira@mjv.com.br>
2019-07-29 02:05:04 -0300

    Ajutes no layout mobile

--- app-gm5/src/components/Header/HeaderComponent.js
+++ app-gm5/src/components/Header/HeaderComponent.js
@@ -5,7 +5,33 @@ import './HeaderComponent.scss'
 
 
 class Header extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            isMenu: ''
+        }
+
+        this.isMenu = this.isMenu.bind(this)
+    }
+
+    isMenu(e){
+        console.log(e.currentTarget.className)
+        e.currentTarget.className === ''?(
+            this.setState({
+                isMenu: 'active'
+            })
+        ):(
+            this.setState({
+                isMenu: ''
+            })
+        )
+    }
+
     render(){
+        const isMenuBox = this.state.isMenu;
+
         return(
             <header>
                 <div className="container">
@@ -13,8 +39,16 @@ class Header extends Component {
                         <div id="logo" className="col-xs-4 col-md-2">
                             <a href="/"><img alt="teste" src={Logo}/></a>
                         </div>
-                        <div className="visible-xs-4 menumob">1</div>
-                        <nav className="col-xs-12 col-md-10">
+                        <div className="visible-xs col-xs-4 visible-sm menumob">
+                            <div className="menu-button">
+                                <a href="#menu" onClick={this.isMenu} className={this.state.isMenu}>
+                                    <span>Menu</span>
+                                    <span></span>
+                                    <span></span>
+                                </a>
+                            </div>
+                        </div>
+                        <nav className={isMenuBox !== ''? 'col-xs-12 col-md-10 open': 'col-xs-12 col-md-10 '}>
                             <ul>
                                 <li>
                                     <a href="/">home</a> 
